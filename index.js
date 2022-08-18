313ff16bd127cd291d6a0a55ee84d8d907a93350
Adarsh Bhutada <106022027+AdarshBhutadaF-E-D@users.noreply.github.com>
2022-08-18 12:15:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,77 @@
-let player1Score = 0;
-let player2Score = 0;
-let player1Turn = true;
+let KanakScore = 0;
+let DevuScore = 0;
+let KanakTurn = true;
 
+const Container = document.querySelector(".container");
 const Message = document.getElementById("message");
+const Msg = document.getElementById("msg");
 const player1 = document.getElementById("player1Scoreboard");
 const player2 = document.getElementById("player2Scoreboard");
+const Players = document.querySelector(".players");
 const player1Dice = document.getElementById("player1dice");
 const player2Dice = document.getElementById("player2dice");
 const RollDice = document.getElementById("RollBtn");
 const ResetGame = document.getElementById("ResetBtn");
+const ChangeDice = document.getElementById("dice");
 
-RollDice.addEventListener("click" , function(){
-   let Random = Math.floor((Math.random() * 6)+ 1);
-     if(player1Turn)
-     {
-        player1Score += Random;
-        player1.textContent = player1Score;
+// function anim(){
+
+// }
+
+
+function showDisplay() {
+    ChangeDice.style.display = "none";
+    RollDice.style.display = "none";
+    ResetGame.style.display = "block";
+    
+    // H1.style.display = "none";
+}
+
+function changeImg(Random) {
+    if (Random === 1) {
+        ChangeDice.style.backgroundImage = "url('/dice-one-solid.svg')";
+    }
+    if (Random === 2) {
+        ChangeDice.style.backgroundImage = "url('/dice-two-solid.svg')";
+    }
+    if (Random === 3) {
+        ChangeDice.style.backgroundImage = "url('/dice-three-solid.svg')";
+    }
+    if (Random === 4) {
+        ChangeDice.style.backgroundImage = "url('/dice-four-solid.svg')";
+    }
+    if (Random === 5) {
+        ChangeDice.style.backgroundImage = "url('/dice-five-solid.svg')";
+    }
+    if (Random === 6) {
+        ChangeDice.style.backgroundImage = "url('/dice-six-solid.svg')";
+    }
+}
+
+RollDice.addEventListener("click", function () {
+    //   anim();
+      Msg.style.display = "none";
+      let Random = Math.floor((Math.random() * 6) + 1);
+      changeImg(Random);
+
+    if (KanakTurn) {
+      
+        KanakScore += Random;
+        player1.textContent = KanakScore;
         player1Dice.textContent = Random;
         player1Dice.classList.remove("active");
         player2Dice.classList.add("active");
-        Message.textContent = ("Player 2 Turn");
+        Message.textContent = ("Devu's Turn");
 
     }
-    else 
-    {
-        player2Score += Random;
-        player2.textContent = player2Score;
+    else {
+        // changeImg(Random);
+        DevuScore += Random;
+        player2.textContent = DevuScore;
         player2Dice.textContent = Random;
         player2Dice.classList.remove("active");
         player1Dice.classList.add("active");
-        Message.textContent = ("Player 1 Turn");
+        Message.textContent = ("Kanak's Turn");
     }
 
     // if(player1Turn){
@@ -39,28 +81,45 @@ RollDice.addEventListener("click" , function(){
     //     player1Turn = true;
     // }
 
-      if(player1Score >= 20){
-        Message.textContent = ("Player 1 Won");
-        RollDice.style.display = "none"; 
-        ResetGame.style.display = "block";
-      }
-      else if(player2Score >= 20){
-        Message.textContent = ("Player 2 Won");
-        RollDice.style.display = "none"; 
-        ResetGame.style.display = "block";
-      }
+    if (KanakScore >= 20) {
+        Message.textContent = ("Congratulations Kanak, you have Won the game❤️🔥");
+        showDisplay();
+        Container.style.backgroundImage = "url('/Tophy1.jpeg')";
 
-    player1Turn = !player1Turn;
+
+    }
+    else if (DevuScore >= 20) {
+        Message.textContent = ("Congratulations Devu, You have won the game👈🏻😉😎");
+        showDisplay();
+        Container.style.backgroundImage = "url('/Tophy1.jpeg')";
+
+    }
+
+    KanakTurn = !KanakTurn;
 });
 
-ResetGame.addEventListener("click" , function(){
-   player1Score = 0;
-   player2Score = 0;
-   player1.textContent = player1Score;
-   player2.textContent = player2Score;
-   player1Dice.classList.add("active");
-   player1Dice.textContent ="-";
-   player2Dice.textContent ="-";
-   Message.textContent = "Player 1 Turn" ;
+ResetGame.addEventListener("click", function () {
+    KanakScore = 0;
+    DevuScore = 0;
+    KanakTurn = true;
+    player1.textContent = 0;
+    player2.textContent = 0;
+    player1Dice.classList.add("active");
+    player1Dice.textContent = "-";
+    player2Dice.textContent = "-";
+    Message.textContent = "Kanak's Turn";
+    Container.style.backgroundImage = "url('/blue.webp')";
+    Msg.style.display = "block";
+    ResetBtn.style.display = "none";
+    RollBtn.style.display = "block";
+    ChangeDice.style.display = "block";
+    ChangeDice.style.backgroundImage = "url('/dice-solid.svg')";
+    // Players.style.display = "block";
+    // player1.style.display = "block";
+    // player2.style.display = "block";
+    // player1Dice.style.display = "block";
+    // player2Dice.style.display = "block";
+    // H1.style.display = "block";
 });
+
 
